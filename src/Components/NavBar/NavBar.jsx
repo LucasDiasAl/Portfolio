67ea85a3da65c1bccc2cae4a680dcc56a4bb26f5
Lucas Dias <lucaalencarID@gmail.com>
2023-04-22 03:38:07 -0300

    header terminado e css tambem

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,15 +3,21 @@ import './NavBar.css';
 function NavBar() {
   return (
     <div className='Nav-Bar'>
-      <div className='Img-Div'>
-        <img src="logo192.png" alt="Icone personalizado" />
+      <div class="Logo-Div">
+        <svg class="Logo-background" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <polygon points="50 0, 95 25, 95 75, 50 100, 5 75, 5 25"/>
+        </svg>
+        <svg class="Logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <polygon points="50 0, 95 25, 95 75, 50 100, 5 75, 5 25" stroke-width="4" />
+          <text x="50" y="50" font-size="60" text-anchor="middle" alignment-baseline="central">D</text>
+        </svg>
       </div>
       <div className='Menu-List-Div'>
         <ol>
-          <li style={{ animationDelay: '0ms' }}>Sobre</li>
-          <li style={{ animationDelay: '100ms' }}>Experiência</li>
-          <li style={{ animationDelay: '200ms' }}>Projetos</li>
-          <li style={{ animationDelay: '300ms' }}>Contato</li>
+          <li style={{ animationDelay: '100ms' }}>Sobre</li>
+          <li style={{ animationDelay: '200ms' }}>Experiência</li>
+          <li style={{ animationDelay: '300ms' }}>Projetos</li>
+          <li style={{ animationDelay: '400ms' }}>Contato</li>
         </ol>
         <div className='Curriculo-Div'>
           <p>
