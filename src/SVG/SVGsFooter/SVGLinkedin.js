26a76f65b573aafbff0a5cda530e03c9ca68f15f
Lucas Dias <lucaalencarID@gmail.com>
2023-04-26 02:42:21 -0300

    addionado svg do github linkedin e email

---
+++ src/SVG/SVGsFooter/SVGLinkedin.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './SVGFooter.css';
+
+function Linkedid () {
+  return (
+    <svg
+      className="Linkedin-link"
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      fill="none"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6z" />
+      <rect x="2" y="9" width="4" height="12" />
+      <circle cx="4" cy="4" r="2" />
+    </svg>
+  );
+}
+
+export default Linkedid;
